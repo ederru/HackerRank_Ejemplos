b872585ad4e3bf36f910011604b77a2214ea2988
Eder Ruiz <0805ederruiz@gmail.com>
2021-09-06 21:14:55 -0500

    Agregamos JavaBigDecimal y documentación

---
+++ src/JavaBigDecimal.java
@@ -0,0 +1,41 @@
+import java.math.BigDecimal;
+import java.util.*;
+
+/*
+Documentación:
+https://docs.oracle.com/javase/7/docs/api/java/math/BigDecimal.html
+https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html
+https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html
+ */
+
+class JavaBigDecimal{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+        Comparator<String> customComparatos = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a);
+            }
+        };
+        Arrays.sort(s, 0, n, customComparatos);
+
+        //Write your code here
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}

00215ac1a3f0c57f6afc85b828306c43f9ed5a59
Eder Ruiz <0805ederruiz@gmail.com>
2021-09-08 22:29:56 -0500

    Agregamos solución al problema Java 1D Array

---
+++ src/Java1DArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Java1DArray {
+
+//Documentación no requerida, sugiero estudio de ciclos y de arreglos.
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int a [] = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
